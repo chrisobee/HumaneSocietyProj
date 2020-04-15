a07227e655fbe0dea79d20399445ac6687d42b49
Chris O'Brien <chrisobrien2000@outlook.com>
2020-04-15 11:48:00 -0500

    Removed unnecessary comments

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -290,7 +290,7 @@ namespace HumaneSociety
         }
         
         // TODO: Animal Multi-Trait Search
-        internal static IQueryable<Animal> SearchForAnimalsByMultipleTraits(Dictionary<int, string> updates) // parameter(s)?
+        internal static IQueryable<Animal> SearchForAnimalsByMultipleTraits(Dictionary<int, string> updates)
         {
 
             IQueryable<Animal> animals = db.Animals;
