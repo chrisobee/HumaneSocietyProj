e6a713bba8b4eb467f50916b94131875fa69df55
Chris O'Brien <chrisobrien2000@outlook.com>
2020-04-15 09:24:32 -0500

    Added logic for UpdateAdoption()

--- HumaneSociety/Query.cs
+++ HumaneSociety/Query.cs
@@ -297,10 +297,6 @@ namespace HumaneSociety
             {
                 animals = FilterAnimals(animals, trait);
             }
-<<<<<<< HEAD
-
-=======
->>>>>>> 321c89eceeec5f3d00cb9ab466ccb6fc021a0c74
             return animals;
         }
         public static IQueryable<Animal> FilterAnimals(IQueryable<Animal> animals, KeyValuePair<int, string> trait)
